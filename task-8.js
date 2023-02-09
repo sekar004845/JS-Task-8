4bb1bcebe440d6fd7a9e6f797bfaff7f2e43cc34
sekar004845 <sekar004845@gmail.com>
2023-02-08 20:53:13 -0800

    update

---
+++ task-8.js
@@ -0,0 +1,300 @@
+                // 1//
+				let bike={
+					number:1,
+					model:"pulsar",
+					speed:120,
+					color:"black",
+					price:20000,
+					year:2017,
+					brand:"honda"
+				}	
+				document.write(JSON.stringify(bike),'<br>');
+				console.log(bike);
+				console.log(bike.model);
+				console.log(bike["price"]);
+				// 2//
+					let g={
+					    number:2,
+						name:'mari',
+						age:24,
+						from:'tenkasi',
+						degree:'ba',
+						mark:99,
+						mark1:90,
+					}
+					
+					document.write(JSON.stringify(g),'<br>');
+					console.log(g);
+					console.log(g.mark1);
+					console.log(g['name']);
+				// 3//
+				let car ={
+					mnumber:3,
+					name:"swift",
+					number:2829,
+					model:2021,
+					company:"Honda",
+					color:"red",
+					type:"four wheeler"
+				}
+				document.write(JSON.stringify(car),'<br>');
+			    console.log(car);
+				console.log(car.company);
+				console.log(car["color"]);
+				// 4//
+				let laptop={
+					number:4,
+					category:"lenevo",
+					type:"window10",
+					price:60000,
+					color:"silver",
+					year:2019,
+					brand:"hp",
+
+				}	
+				document.write(JSON.stringify(laptop),'<br>');
+				console.log(laptop);
+				console.log(laptop.category);
+				console.log(laptop["type"]);
+				// 5//
+				let animal={
+					number:5,
+					category:"cat",
+					type:"whild animal",
+					color:"ash",
+					price:2000,
+					name:"pri",
+					brand:"teddy",
+				}
+				document.write(JSON.stringify(animal),'<br>');	
+				console.log(animal);
+				console.log(animal.color);
+				console.log(animal["name"]);
+			  // 6//
+			  let bag={
+			  number:6,
+			  model:"fashion",
+			  size:"big",
+			  type:3,
+			  chip:5,
+			  cloth:"very thick",
+			}
+			document.write(JSON.stringify(bag),'<br>');
+			console.log(bag);
+			console.log(bag.cloth);
+			console.log(bag["size"]);
+				// 7//
+				let mobile={
+					number:7,
+					model:"note 7 pro",
+					type:"android",
+					color:"blue",
+					price:20000,
+					year:2017,
+					brand:"redmi"
+				}	
+				document.write(JSON.stringify(mobile),'<br>');
+				console.log(mobile);
+				console.log(mobile.type);
+				console.log(mobile["price"]);
+				
+				// 8//
+				let paint={
+					number:8,
+					category:"wall paint",
+					type:"waterproof",
+					price:500,
+					color:"purple",
+					year:2000,
+					brand:"asian paints",
+
+				}	
+				document.write(JSON.stringify(paint),'<br>');
+				console.log(paint);
+				console.log(paint.category);
+				console.log(paint["type"]);
+				// 9//
+				let toys={
+					number:9,
+					category:"kitchenset",
+					items:2,
+					user:"child",
+					color:"pink",
+					price:200,
+					brand:"aphm"
+				}	
+				document.write(JSON.stringify(toys),'<br>');
+				console.log(toys);
+				console.log(toys.category);
+				console.log(toys["brand"]);
+				
+				// 10//
+				
+					let d={
+					    number:10,
+						name:'kabil',
+						age:54,
+						from:'nellai',
+						degree:'be',
+						mark:90,
+						mark1:90,
+					}
+					
+					document.write(JSON.stringify(d),'<br>');
+					console.log(d);
+					console.log(d.mark1);
+					console.log(d['name']);
+				// 11//
+				let book={
+					number:11,
+					category:"c",
+					offter:30,
+					section:"computer science",
+					price:4000,
+					 edition:3,
+					 author:"jane",
+				}	
+				document.write(JSON.stringify(book),'<br>');
+				console.log(book);
+				console.log(book.edition);
+				console.log(book["author"]);
+				// 12//
+				let student={
+					number:12,
+					name:"venila",
+					totel:450,
+					sub:"science",
+					contact:9796978696,
+					age:22,
+
+				}	
+				document.write(JSON.stringify(student),'<br>');
+				console.log(student);
+				console.log(student.total);
+				console.log(student["name"]);
+				
+				// 13//
+					let h={
+					number:13,
+					name:'sakithe',
+					age:28,
+					from:'karur',
+					degree:'ba',
+					mark:99,
+					mark1:80,
+				}
+				
+				document.write(JSON.stringify(h),'<br>');
+				console.log(h);
+				console.log(h.mark1);
+				console.log(h['name']);
+				// 14//
+				let notes={
+					number:14,
+					category:"school bag",
+					type:"leather",
+					price:500,
+					color:"all color",
+					user:" students",
+					brand:"prisa",
+
+				}	
+				document.write(JSON.stringify(notes),'<br>');
+				console.log(notes);
+				console.log(notes.price);
+				console.log(notes["user"]);
+				
+				
+				
+				
+				
+				// fifteen//
+				let shirts={
+					number:15,
+					category:"men shirt",
+					type:"full hand",
+					price:1200,
+					color:"white",
+					brand:"otto",
+				}	
+				document.write(JSON.stringify(shirts),'<br>');
+				console.log(shirts);
+				console.log(shirts.category);
+				console.log(shirts["color"]);
+				
+
+				
+				
+				// 16//
+				let spices={
+					number:16,
+					measurement:"kg",
+					name:"pepper",
+					price:20,
+					color:"black",
+					uses:"food"
+					
+				}	
+				document.write(JSON.stringify(spices),'<br>');
+				console.log(spices);
+				console.log(spices.color);
+				console.log(spices["uses"]);
+				
+				// 17//
+				let food={
+					number:17,
+					category:"break fast",
+					name:"dosa",
+					ingrediants:"rice",
+					color:"Red",
+					nation:"india",
+					
+				}	
+				document.write(JSON.stringify(food),'<br>');
+				console.log(food);
+				console.log(food.color);
+				console.log(food["ingrediants"]);
+				
+				// 18//
+				let bird={
+					number:18,
+					category:"national bird",
+					name:"peacock",
+					ingrediants:"rice",
+					color:"blue with green",
+					nation:"india",
+					
+				}	
+				document.write(JSON.stringify(bird),'<br>');
+				console.log(bird);
+				console.log(bird.color);
+				console.log(bird["name"]);
+				// 19//
+				let b={
+				number:19,
+				name:'kumar',
+				age:24,
+				from:'tenkasi',
+				degree:'bba',
+				mark:90,
+				mark1:70,
+			}
+			
+			document.write(JSON.stringify(b),'<br>');
+			console.log(b.mark);
+			console.log(b);
+			console.log(b['name']);
+			// 20//
+				let paste={
+					number:20,
+					category:"toothpaste",
+					name:"colgate",
+					price:20,
+					ingrediants:"Salt",
+					owner:"james",
+					
+				}	
+				document.write(JSON.stringify(paste),'<br>');
+				console.log(paste);
+				console.log(paste.category);
+				console.log(paste["category"]);
